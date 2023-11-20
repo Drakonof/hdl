f192fda5d0e0ab85789c8993f500a5bc7f20f562
Drakonof <artyom.shimko@gmail.com>
2023-11-20 16:08:34 +0400

    add: created

--- rtl/sync_fifo.sv
+++ rtl/sync_fifo.sv
@@ -1,151 +1,156 @@
-/**
- * \file sync_fifo.sv
- * \author A. Shimko
- * \brief Synchronous fifo.
- *
- * \code{.unparsed}
- * 
- * sync_fifo # 
- * (
- *     .DATA_WIDTH       (),
- *     .ADDR_WIDTH       (),
- * 
- *     .RAM_TYPE         (),
- * 
- *     .ALMOST_FULL_VAL  (),
- *     .ALMOST_EMPTY_VAL (),
- * )
- * sync_fifo_inst                         
- * (
- *     .clk_i          (),
- *     .s_rst_n_i      (),
- * 
- *     .wr_en_i        (),
- *     .wr_data_i      (),
- *     .almost_full_o  (),
- *     .full_o         (),
- * 
- *     .rd_en_i        (),
- *     .rd_data_o      (),
- *     .almost_empty_o (),
- *     .empty_o        ()
- * );
- *
- *  \endcode
- *
- * 15.12.21 : add (sync_fifo.sv): created
- * 23.12.21 : fix (sync_fifo.sv): the FIFO_DEPTH parameter was both 
- *          1. replaced to a ADDR_WIDTH parameter and 
- *          2. moved to localparam from parameter
- * 12.03.22 : fix (sync_fifo.sv): all '1 constructions were replaced to 1'h1
- * 01.04.22 : fix (sync_fifo.sv): fully rewritten
- * 28.04.22 : fix (sync_fifo.sv): changed if statements from not synthesized place to asserts
- * 19.05.22 : fix (sync_fifo.sv): change the comments to doxygen style ones
- *
- */
+//`include "platform.vh"
 
-`timescale 1ns / 1ps
 
-module sync_fifo #
+`timescale 1 ns / 1 ps
+
+
+`ifdef XILINX
+  `resetall
+  `default_nettype none
+`endif
+
+
+module async_fifo #
 (
-    /** \name Parameters. @{ */
-    parameter unsigned DATA_WIDTH         = 8,
-    
-    parameter unsigned ADDR_WIDTH         = 8,
-    
-    parameter string        RAM_TYPE           = "block", /**< Ram type: "distributed", "block". */  
+  parameter unsigned DATA_WIDTH   = 32,
 
-    parameter unsigned ALMOST_FULL_VAL  = 2, /**< Hom much of words to an full state. */
-    parameter unsigned ALMOST_EMPTY_VAL = 2, /**< Hom much of words to an empty state. */
+  parameter unsigned ADDR_WIDTH   = 8,
 
-    /** @} \name Local parameters. @{ */  
-    localparam unsigned FIFO_DEPTH = (2 ** ADDR_WIDTH)
-    /** @} */
+  parameter unsigned ALMOST_FULL  = 2,
+  parameter unsigned ALMOST_EMPTY = 2,
+
+`ifdef XILINX
+  parameter          RAM_STYLE    = "auto" // "distributed", "block", "registers", "ultra", "mixed", "auto"
+`endif
 )
 (
-    /** \name Signals. @{ */   
-    input  logic                      clk_i,
-    input  logic                      s_rst_n_i,
-    
-    input  logic                      wr_en_i,
-    input  logic [DATA_WIDTH - 1 : 0] wr_data_i,
-    output logic                      almost_full_o,
-    output logic                      full_o,
-    
-    input  logic                      rd_en_i,
-    output logic [DATA_WIDTH - 1 : 0] rd_data_o,
-    output logic                      almost_empty_o,
-    output logic                      empty_o
-    /** @} */
+  input  logic                      clk_i,
+
+  input  logic                      s_rst_n_i,
+
+  input  logic                      wr_en_i,
+  input  logic [DATA_WIDTH - 1 : 0] data_i,
+  output logic                      almost_full_o,
+  output logic                      full_o,
+
+  input  logic                      rd_en_i,
+  output logic [DATA_WIDTH - 1 : 0] data_o,
+  output logic                      almost_empty_o,
+  output logic                      empty_o
 );
-    /** \name Local parameters. @{ */
-    localparam unsigned A_FULL        = FIFO_DEPTH - ALMOST_FULL_VAL; /**< Almost full. It's number words before full condition. */
-    localparam unsigned A_EMPTY       = ALMOST_EMPTY_VAL; /**< Almost empty. It's number words before empty condition. */
 
-    /** @} \name Internal signals and regs. @{ */
-    logic [ADDR_WIDTH - 1 : 0] wr_addr;
-    logic [ADDR_WIDTH - 1 : 0] rd_addr;
 
-    logic [ADDR_WIDTH : 0]     wr_pointer;
-    logic [ADDR_WIDTH : 0]     rd_pointer;
+  localparam unsigned FIFO_DEPTH = (2 ** ADDR_WIDTH);
 
-    /** \brief Choosing ram type attribute for fifo memory. */
-    (*ram_style = RAM_TYPE*) 
-    logic [DATA_WIDTH - 1 : 0] mem [0 : FIFO_DEPTH - 1] ; 
-    /** @} */
+  initial
+    begin
+    if ((ALMOST_FULL > FIFO_DEPTH) || (ALMOST_FULL < 1))
+      begin
+          $error("Incorrect parameter value: ALMOST_FULL");    
+      end 
 
-    /** \name Behavioral block. @{ */
-    always_comb begin
-        wr_addr        = wr_pointer [ADDR_WIDTH - 1 : 0];
-        rd_addr        = rd_pointer [ADDR_WIDTH - 1 : 0];
+    if ((ALMOST_EMPTY > FIFO_DEPTH) || (ALMOST_EMPTY < 1))
+      begin
+        $error("Incorrect parameter value: ALMOST_EMPTY_VALUE");
+      end
 
-        full_o         = (wr_pointer != rd_pointer) && (wr_addr == rd_addr); /**< Empty signal generation. */
-        empty_o        = (wr_pointer == rd_pointer) && (wr_addr == rd_addr); /**< Full signal generation. */
+    if (ADDR_WIDTH == 0)
+      begin
+        $error("Incorrect parameter value: ADDR_WIDTH");
+      end
 
-        almost_full_o  = (wr_pointer - rd_pointer) >= A_FULL;  /**< Almost full signal generation. */
-        almost_empty_o = (wr_pointer - rd_pointer) <= A_EMPTY; /**< Almost empty signal generation. */
+    if (DATA_WIDTH == 0)
+      begin
+        $error("Incorrect parameter value: ADDR_WIDTH");
+      end
+  end
 
-        rd_data_o      = mem[rd_addr]; /**< Data reading. */
+  localparam unsigned A_FULL     = FIFO_DEPTH - ALMOST_FULL;
+  localparam unsigned A_EMPTY    = ALMOST_EMPTY;
+
+  logic [ADDR_WIDTH : 0]     wr_pointer; // one bit extra
+  logic [ADDR_WIDTH : 0]     rd_pointer; // one bit extra
+
+  logic [ADDR_WIDTH - 1 : 0] wr_addr;
+  logic [ADDR_WIDTH - 1 : 0] rd_addr;
+
+  logic                      full;
+  logic                      empty;
+
+`ifdef XILINX
+  (*ram_style = RAM_STYLE*)
+`endif
+  logic [DATA_WIDTH - 1 : 0] mem [0 : FIFO_DEPTH - 1];
+
+
+  always_comb
+    begin
+      wr_addr = wr_pointer [ADDR_WIDTH - 1 : 0];
+      rd_addr = rd_pointer [ADDR_WIDTH - 1 : 0];
     end
-  
-    /** Poiner of writing logic. */
-    always_ff @ (posedge clk_i) begin : wr_pointer_control
-        if (s_rst_n_i == 1'b0) begin
-            wr_pointer <= '0;
-        end 
-        else if ((wr_en_i == 1'b1) && (full_o == 1'b0)) begin
-            wr_pointer <= wr_pointer + 1'b1;
+
+  always_comb
+    begin
+      full           = (wr_pointer != rd_pointer) && (wr_addr == rd_addr);
+      full_o         = full;
+      almost_full_o  = (wr_pointer - rd_pointer) >= A_FULL;
+
+
+      empty          = (wr_pointer == rd_pointer) && (wr_addr == rd_addr);
+      empty_o        = empty;
+      almost_empty_o = (wr_pointer - rd_pointer) <= A_EMPTY;
+    end
+
+  always_ff @(posedge clk_i)
+    begin
+      if (s_rst_n_i == 'h0) 
+        begin
+          wr_pointer      <= '0;
+          wr_gray_pointer <= '0;
+        end
+      else if ((wr_en_i == 'h1) && (full != 'h1))
+        begin
+          wr_pointer <= wr_pointer + 'h1;
         end
     end
-    
-    /** Poiner of reading logic. */
-    always_ff @ (posedge clk_i) begin  : rd_pointer_control
-        if (s_rst_n_i == 1'b0) begin
-            rd_pointer <= '0;
-        end 
-        else if ((rd_en_i == 1'b1) && (empty_o == 1'b0)) begin
-            rd_pointer <= rd_pointer + 1'b1;
-        end
-    end
-   
-    /** Data writing logic. */
-    always_ff @ (posedge clk_i) begin  : wr_data
-        if ((wr_en_i == 1'b1) && (full_o == 1'b0)) begin
-            mem[wr_addr] <= wr_data_i;
-        end
-    end
-    /** @} */
 
-    /** \name Not synthesized block. @{ */
-    always @ (posedge clk_i) begin
-        assert ((wr_en_i == 1'b1) && (full_o == 1'b1)) begin
-            $display("full fifo is being written ");
+  always_ff @(posedge clk_i)
+    begin
+      if (s_rst_n_i == 'h0) 
+        begin
+          rd_pointer <= '0;
         end
-
-        assert ((rd_en_i == 1'b1) && (empty_o == 1'b1)) begin
-            $display("empty fifo is being read");
+      else if ((rd_en_i == 'h1) && (empty != 'h1))
+        begin
+          rd_pointer <= rd_pointer + 'h1;
         end
     end
-    /** @} */
+
+  always_comb
+    begin
+      data_o = mem[rd_addr];
+    end
+
+  always_ff @(posedge clk_i)
+    begin
+      if ((wr_en_i == 'h1) && (full != 'h1))
+        begin
+          mem[wr_addr] <= data_i;
+        end
+    end
+
+  always @(posedge clk_i)
+    begin
+      assert ((wr_en_i == 'h1) && (full == 'h1)) 
+        begin
+          $display("full fifo is being written ");
+        end
+
+      assert ((rd_en_i == 'h1) && (empty == 'h1))
+        begin
+          $display("empty fifo is being read");
+        end
+    end
+
 
 endmodule
